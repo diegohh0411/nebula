7284c5c054a62f0ca20f9e95e3c9464081a5f06d
Diego Hernandez <diegohh0411@gmail.com>
2026-04-17 13:14:51 -0600

    refactor: remove redundant inner Arc wrappers from VisionEngine

--- src-tauri/src/vision_engine.rs
+++ src-tauri/src/vision_engine.rs
@@ -1,21 +1,20 @@
 use ort::session::Session;
-use std::sync::Arc;
 use std::path::PathBuf;
 
 pub struct VisionEngine {
-    data_dir: PathBuf,
-    image_session: Arc<std::sync::Mutex<Option<Session>>>,
-    text_session: Arc<std::sync::Mutex<Option<Session>>>,
-    tokenizer: Arc<std::sync::Mutex<Option<tokenizers::Tokenizer>>>,
+    pub data_dir: PathBuf,
+    image_session: std::sync::Mutex<Option<Session>>,
+    text_session: std::sync::Mutex<Option<Session>>,
+    tokenizer: std::sync::Mutex<Option<tokenizers::Tokenizer>>,
 }
 
 impl VisionEngine {
     pub fn new(data_dir: PathBuf) -> Self {
         Self {
             data_dir,
-            image_session: Arc::new(std::sync::Mutex::new(None)),
-            text_session: Arc::new(std::sync::Mutex::new(None)),
-            tokenizer: Arc::new(std::sync::Mutex::new(None)),
+            image_session: std::sync::Mutex::new(None),
+            text_session: std::sync::Mutex::new(None),
+            tokenizer: std::sync::Mutex::new(None),
         }
     }
 }
